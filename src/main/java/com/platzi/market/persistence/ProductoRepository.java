335c71e6a7de8dab0ca3b5880068c585d378cc2d
Maria Fernanda Orozco D <you@example.com>
2023-05-04 10:45:33 -0500

    Avance nro 20

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository //Esta clase interactua con la BD, desde aca se ejecutan las operaciones para las tablas
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -15,8 +17,20 @@ public class ProductoRepository {
     public List<Producto> getByCategoria(int idCategoria){
         return productoCrudRepository.findByidCategoriaOrderByNombreAsc(idCategoria);
     }
-    
+
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
